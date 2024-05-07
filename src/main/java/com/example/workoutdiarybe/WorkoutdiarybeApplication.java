81f323349368d7bd3d9718b75441fae40c3c9fe8
Jacopo Capudi <jacopo.capudi@euris.it>
2024-05-07 13:26:12 +0200

    Update WorkoutdiarybeApplication.java

--- src/main/java/com/example/workoutdiarybe/WorkoutdiarybeApplication.java
+++ src/main/java/com/example/workoutdiarybe/WorkoutdiarybeApplication.java
@@ -1,9 +1,16 @@
 package com.example.workoutdiarybe;
 
+import java.util.Arrays;
+
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
 @SpringBootApplication
 @ComponentScan
@@ -12,6 +19,22 @@ public class WorkoutdiarybeApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(WorkoutdiarybeApplication.class, args);
+	}
+
+	@Bean
+	public CorsFilter corsFilter() {
+		CorsConfiguration corsConfiguration = new CorsConfiguration();
+		corsConfiguration.setAllowCredentials(true);
+		corsConfiguration.setAllowedOrigins(Arrays.asList("http://localhost:4200"));
+		corsConfiguration.setAllowedHeaders(Arrays.asList("Origin", "Access-Control-Allow-Origin", 
+			"Content-Type", "Accept","Authorization", "Origin, Accept", 
+			"X-Request-Width", "Access-Control-Request-Method", "Access-Control-Request-Headers"));
+		corsConfiguration.setExposedHeaders(Arrays.asList("Origin", 
+		"Content-Type", "Accept","Authorization", "Access-Control-Allow-Origin", "Access-Control-Allow-Credentials"));
+		corsConfiguration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+		UrlBasedCorsConfigurationSource urlBasedCorsConf = new UrlBasedCorsConfigurationSource();
+		urlBasedCorsConf.registerCorsConfiguration("/**", corsConfiguration);
+		return new CorsFilter(urlBasedCorsConf);
 	}
 
 }
