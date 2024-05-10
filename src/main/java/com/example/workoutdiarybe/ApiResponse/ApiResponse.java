79896544fe16559a3fa145587a223caf12ef740d
Jacopo Capudi <jacopo.capudi@euris.it>
2024-05-10 09:29:51 +0200

    Update ApiResponse.java

--- src/main/java/com/example/workoutdiarybe/ApiResponse/ApiResponse.java
+++ src/main/java/com/example/workoutdiarybe/ApiResponse/ApiResponse.java
@@ -3,9 +3,11 @@ package com.example.workoutdiarybe.ApiResponse;
 public class ApiResponse {
     private String status;
     private String message;
+    private String contentType;
 
-    public ApiResponse(String status, String message){
+    public ApiResponse(String status, String message, String contentType){
         this.status = status;
         this.message = message;
+        this.contentType = contentType;
     }
 }
